63e3c0d71692c84f69b2368d262ebdb73f0f5e0b
yasaraswat@deloitte.com <yasaraswat@deloitte.com>
2023-02-24 13:55:07 +0530

    End of Section 2

---
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,17 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid  Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+
+        public Guid RegionId { get; set; }
+        public string WalkDifficulltybyId { get; set; }
+
+        //Navigation Properties
+
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
